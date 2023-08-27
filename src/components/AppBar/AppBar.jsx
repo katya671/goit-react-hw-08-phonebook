ab764ab27465731ae50a60d5e823f79537984528
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-08-27 09:19:43 +0300

    Update styles

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import UserMenu from 'components/UserMenu/UserMenu';
 import css from './AppBar.module.css';
 import AuthNav from 'components/AuthNav/AuthNav';
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/selectors';
+
+const StyledLink = styled(NavLink)`
+  color: var(--main);
+
+  &.active {
+    color: var(--main-hover);
+  }
+`;
 
 const AppBar = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
@@ -13,13 +22,13 @@ const AppBar = () => {
     <header className={css.header}>
       <div className={css.container}>
         <div className={css.nav}>
-          <NavLink className={css.link} to="/">
+          <StyledLink className={css.link} to="/">
             Home
-          </NavLink>
+          </StyledLink>
           {isLoggedIn && (
-            <NavLink className={css.link} to="/contacts">
+            <StyledLink className={css.link} to="/contacts">
               Contacts
-            </NavLink>
+            </StyledLink>
           )}
         </div>
         {isLoggedIn ? <UserMenu /> : <AuthNav />}
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
     <div style={{ textAlign: 'center' }}>
       <ReactLoading
         type="spinningBubbles"
-        color="#ea84a4"
+        color="var(--main)"
         height={50}
         width={50}
         className={css.loading}
